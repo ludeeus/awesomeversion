150ae49c820df00a2f06de65457b9dbc021fe811
Ludeeus <ludeeus@ludeeus.dev>
2020-12-29 11:58:32 +0000

    Add special handler for containe tag compare

--- awesomeversion/strategy.py
+++ awesomeversion/strategy.py
@@ -1,5 +1,21 @@
 """Strategies for AwesomeVersion."""
+import re
+from dataclasses import dataclass
 from enum import Enum
+
+RE_SPECIAL_CONTAINER = re.compile(r"^(latest|dev|stable|beta)$")
+
+
+@dataclass
+class SpecialHandler:
+    re: re.Pattern
+    map: dict
+
+
+class SpecialHandlers:
+    container = SpecialHandler(
+        RE_SPECIAL_CONTAINER, {"stable": 1, "beta": 2, "latest": 3, "dev": 4}
+    )
 
 
 class AwesomeVersionStrategy(str, Enum):
@@ -9,4 +25,5 @@ class AwesomeVersionStrategy(str, Enum):
     CALVER = "CalVer"
     SEMVER = "SemVer"
     SIMPLEVER = "SimpleVer"
+    SPECIALCONTAINER = "SpecialContainer"
     UNKNOWN = "unknown"
--- tests/test_match.py
+++ tests/test_match.py
@@ -29,5 +29,14 @@ def test_simplever():
 
 def test_unknown():
     """Test unknown."""
-    assert version_strategy("latest") == AwesomeVersionStrategy.UNKNOWN
+    assert version_strategy("string") == AwesomeVersionStrategy.UNKNOWN
     assert version_strategy("20.1.0") != AwesomeVersionStrategy.UNKNOWN
+
+
+def test_specialcontainer():
+    """Test specialcontainer."""
+    assert version_strategy("latest") == AwesomeVersionStrategy.SPECIALCONTAINER
+    assert version_strategy("stable") == AwesomeVersionStrategy.SPECIALCONTAINER
+    assert version_strategy("beta") == AwesomeVersionStrategy.SPECIALCONTAINER
+    assert version_strategy("dev") == AwesomeVersionStrategy.SPECIALCONTAINER
+    assert version_strategy("20.1.0") != AwesomeVersionStrategy.SPECIALCONTAINER
