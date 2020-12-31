cdb71e82fd724a908e5dfd2c9d4d72564e3b3d66
Joakim Sørensen <hi@ludeeus.dev>
2020-12-31 05:11:16 +0100

    Test code examples provided in issues (#17)
    * Generate test from issue
    
    * add requirements
    
    * fix requirements
    
    * fix syntax
    
    * Add label and comment
    
    * add assignee
    
    * fix command
    
    * test
    
    * good enough

---
+++ scripts/create_test_from_issue.py
@@ -0,0 +1,39 @@
+"""Generate test for github issue."""
+import sys
+import requests
+
+URL = "https://api.github.com/repos/ludeeus/awesomeversion/issues/{number}"
+
+ISSUE_CONTENT = """\"\"\"Test for issue #{number}.\"\"\"
+# https://github.com/ludeeus/awesomeversion/issues/{number}
+{imports}
+
+def test():
+    \"\"\"Test for issue #{number}.\"\"\"
+    {content}
+"""
+issue_number = sys.argv[1]
+imports = []
+content = []
+request = requests.get(URL.format(number=issue_number))
+body = request.json()["body"].split("```python")[1].split("```")[0]
+
+for line in body.split("\r\n"):
+    if "from " and " import " in line:
+        imports.append(line)
+    elif not line:
+        continue
+    else:
+        content.append(line)
+
+if len(content) == 0:
+    sys.exit(1)
+
+with open(f"./tests/issues/test_issue{issue_number}.py", "w") as target:
+    target.write(
+        ISSUE_CONTENT.format(
+            number=issue_number,
+            imports="\n".join(imports),
+            content="\n    ".join(content),
+        )
+    )
--- tests/issues/test_issue14.py
+++ tests/issues/test_issue14.py
@@ -1,9 +1,11 @@
+"""Test for issue #14."""
 # https://github.com/ludeeus/awesomeversion/issues/14
 from awesomeversion.strategy import AwesomeVersionStrategy
 from awesomeversion import AwesomeVersion
 
 
 def test():
+    """Test for issue #14."""
     version = AwesomeVersion("2021.1.0.dev20201230")
     assert version.dev
     assert version.strategy != AwesomeVersionStrategy.UNKNOWN
