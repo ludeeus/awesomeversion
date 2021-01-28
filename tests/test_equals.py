b4e46ab61e154651d83867cf9bbb680cc9ce4c00
Ludeeus <ludeeus@ludeeus.dev>
2021-01-28 09:43:59 +0000

    Add more tests

--- tests/test_equals.py
+++ tests/test_equals.py
@@ -9,3 +9,7 @@ def test_equals():
     assert AwesomeVersion("2020") == AwesomeVersion("v2020")
     assert AwesomeVersion("2020") == AwesomeVersion("V2020")
     assert AwesomeVersion("2020") != AwesomeVersion("2021")
+    assert str(AwesomeVersion("2021.2.0b0")) == "2021.2.0b0"
+    assert AwesomeVersion("2021.2.0b0").string == "2021.2.0b0"
+    assert str(AwesomeVersion("2021.2.0.dev20210118")) == "2021.2.0.dev20210118"
+    assert AwesomeVersion("2021.2.0.dev20210118").string == "2021.2.0.dev20210118"
