bf96adb92a7051d19cf6ba3cfd467ffb52ef2c73
Ludeeus <ludeeus@ludeeus.dev>
2021-01-30 12:21:24 +0000

    add v

--- tests/test_version_scheme.py
+++ tests/test_version_scheme.py
@@ -10,6 +10,7 @@ from awesomeversion.strategy import AwesomeVersionStrategy
     [
         ("0.118.0", AwesomeVersionStrategy.SEMVER, False, False, None, None),
         ("1.0.0-beta.1", AwesomeVersionStrategy.SEMVER, False, True, "beta.1", "beta"),
+        ("v1.0.0-beta.1", AwesomeVersionStrategy.SEMVER, False, True, "beta.1", "beta"),
         ("2021.2.0.dev1", AwesomeVersionStrategy.CALVER, True, False, "dev1", "dev"),
         ("stable", AwesomeVersionStrategy.SPECIALCONTAINER, False, False, None, None),
     ],
@@ -17,7 +18,7 @@ from awesomeversion.strategy import AwesomeVersionStrategy
 def test_version_scheme(version, strategy, dev, beta, modifier, modifier_type):
     """Test that the version matches the expected scheme."""
     version_object = AwesomeVersion(version)
-    assert version_object.string == version
+    assert str(version_object) == version
     assert version_object.strategy == strategy
     assert version_object.dev == dev
     assert version_object.beta == beta
