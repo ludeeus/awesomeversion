5b0805037e82bfffd72f0f5da9c9b17117803325
Ludeeus <ludeeus@ludeeus.dev>
2021-10-23 09:26:02 +0000

    Use parametrize in compare hendler tests

--- tests/test_strategy.py
+++ tests/test_strategy.py
@@ -5,24 +5,50 @@ from awesomeversion import AwesomeVersion
 from awesomeversion.strategy import AwesomeVersionStrategy
 from awesomeversion.typing import VersionType
 
+BUILDVER = (
+    "1",
+    "123",
+)
+CALVER = (
+    "20.1.0",
+    "20.1",
+)
+PEP440 = (
+    "1.0a1",
+    "1.0.dev1",
+    "1.0b2.post345.dev456",
+)
+SEMVER = (
+    "0.118.0",
+    "1.2.3",
+)
+SIMPLEVER = (
+    "1.0",
+    "1.2.3.4.5",
+)
+SPECIALCONTAINER = (
+    "beta",
+    "dev",
+    "latest",
+    "stable",
+)
+UNKNOWN = (
+    "",
+    "unknown",
+    None,
+)
+
 
 @pytest.mark.parametrize(
     "version,strategy",
     [
-        ("0.118.0", AwesomeVersionStrategy.SEMVER),
-        ("1.2.3.4.5", AwesomeVersionStrategy.SIMPLEVER),
-        ("1", AwesomeVersionStrategy.BUILDVER),
-        ("1.0", AwesomeVersionStrategy.SIMPLEVER),
-        ("v1.0", AwesomeVersionStrategy.SIMPLEVER),
-        ("123", AwesomeVersionStrategy.BUILDVER),
-        ("20.1.0", AwesomeVersionStrategy.CALVER),
-        ("beta", AwesomeVersionStrategy.SPECIALCONTAINER),
-        ("dev", AwesomeVersionStrategy.SPECIALCONTAINER),
-        ("latest", AwesomeVersionStrategy.SPECIALCONTAINER),
-        ("stable", AwesomeVersionStrategy.SPECIALCONTAINER),
-        ("", AwesomeVersionStrategy.UNKNOWN),
-        ("unknown", AwesomeVersionStrategy.UNKNOWN),
-        (None, AwesomeVersionStrategy.UNKNOWN),
+        *[(v, AwesomeVersionStrategy.BUILDVER) for v in BUILDVER],
+        *[(v, AwesomeVersionStrategy.CALVER) for v in CALVER],
+        *[(v, AwesomeVersionStrategy.PEP440) for v in PEP440],
+        *[(v, AwesomeVersionStrategy.SEMVER) for v in SEMVER],
+        *[(v, AwesomeVersionStrategy.SIMPLEVER) for v in SIMPLEVER],
+        *[(v, AwesomeVersionStrategy.SPECIALCONTAINER) for v in SPECIALCONTAINER],
+        *[(v, AwesomeVersionStrategy.UNKNOWN) for v in UNKNOWN],
     ],
 )
 def test_strategy_match(version: VersionType, strategy: AwesomeVersionStrategy) -> None:
