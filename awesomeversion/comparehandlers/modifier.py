7aa6b0cfc4eeb797be6b15e7df32fb50afede7f0
Ludeeus <ludeeus@ludeeus.dev>
2021-12-24 10:59:01 +0000

    lint

--- awesomeversion/comparehandlers/modifier.py
+++ awesomeversion/comparehandlers/modifier.py
@@ -20,31 +20,37 @@ def compare_handler_semver_modifier(
     version_b: "AwesomeVersion",
 ) -> Optional[bool]:
     """Compare handler sections."""
-    if AwesomeVersionStrategy.SEMVER not in (version_a.strategy, version_b.strategy):
+    if (
+        AwesomeVersionStrategy.SEMVER
+        not in (
+            version_a.strategy,
+            version_b.strategy,
+        )
+        or (version_a.modifier_type is None or version_b.modifier_type is None)
+    ):
         return None
 
-    if version_a.modifier_type is not None and version_b.modifier_type is not None:
-        if version_a.modifier_type != version_b.modifier_type:
-            return (
-                SEMVER_MODIFIER_MAP[version_a.modifier_type]
-                > SEMVER_MODIFIER_MAP[version_b.modifier_type]
-            )
+    if version_a.modifier_type != version_b.modifier_type:
+        return (
+            SEMVER_MODIFIER_MAP[version_a.modifier_type]
+            > SEMVER_MODIFIER_MAP[version_b.modifier_type]
+        )
 
-        ver_a_modifier = get_regex_match_group(
-            RE_MODIFIER,
-            get_regex_match_group(
-                generate_full_string_regex(RE_SEMVER), version_a.string, 4
-            ),
-            4,
-        )
-        ver_b_modifier = get_regex_match_group(
-            RE_MODIFIER,
-            get_regex_match_group(
-                generate_full_string_regex(RE_SEMVER), version_b.string, 4
-            ),
-            4,
-        )
-        if not ver_a_modifier:
-            return True
-        if ver_b_modifier:
-            return int(ver_a_modifier) > int(ver_b_modifier)
+    ver_a_modifier = get_regex_match_group(
+        RE_MODIFIER,
+        get_regex_match_group(
+            generate_full_string_regex(RE_SEMVER), version_a.string, 4
+        ),
+        4,
+    )
+    ver_b_modifier = get_regex_match_group(
+        RE_MODIFIER,
+        get_regex_match_group(
+            generate_full_string_regex(RE_SEMVER), version_b.string, 4
+        ),
+        4,
+    )
+    if not ver_a_modifier:
+        return True
+    if ver_b_modifier:
+        return int(ver_a_modifier) > int(ver_b_modifier)
